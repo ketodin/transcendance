791c08700d1072b2a282a61caa505cb707710338
Timeo Daclin <tdaclin@student.42lyon.fr>
2026-04-30 04:09:18 +0200

    feat(db): add the database connection logic

---
+++ src/lib/server/db.ts
@@ -0,0 +1,10 @@
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "$lib/prisma/client";
+import { env } from "$env/dynamic/private";
+
+const url = env.DATABASE_URL ?? "file:./locale.db"
+const adapter = new PrismaBetterSqlite3({ url: url });
+const db = new PrismaClient({ adapter });
+
+export default db;
+
