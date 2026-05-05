074caf6ce86bd2e974df7cf052b0e6dc43d5a3c1
Timeo Daclin <tdaclin@student.42lyon.fr>
2026-05-05 21:03:45 +0200

    fix(db): change default db file to dev.db

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,7 +2,7 @@ import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "$lib/prisma/client";
 import { env } from "$env/dynamic/private";
 
-const url = env.DATABASE_URL ?? "file:./locale.db"
+const url = env.DATABASE_URL ?? "file:./dev.db"
 const adapter = new PrismaBetterSqlite3({ url: url });
 const db = new PrismaClient({ adapter });
 
