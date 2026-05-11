1cf335b4031c604da311f450a653cb47cd6848fc
pabellis <mail@bellissantpablo.fr>
2026-05-11 18:21:06 +0200

    added privacy policy page

--- src/routes/legal/privacy/+page.svelte
+++ src/routes/legal/privacy/+page.svelte
@@ -1,1 +1,13 @@
-Privacy policy
+<script lang="ts">
+	import { m } from "$lib/paraglide/messages";
+</script>
+
+<h1 class="terms">
+	{m.privacy_policy()}
+</h1>
+
+<style>
+	.terms {
+		white-space: pre-line;
+	}
+</style>
