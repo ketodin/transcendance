e3e24d152e0e23d6b4665c4027f285ac3a46fc31
Oliver Kientzler <okientzl@z4r11p6.42lyon.fr>
2026-05-13 17:17:02 +0200

    feat(auth): add middleware

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,10 @@
 import type { Handle } from '@sveltejs/kit';
 import { getTextDirection } from '$lib/paraglide/runtime';
 import { paraglideMiddleware } from '$lib/paraglide/server';
+import { auth } from "$lib/server/auth";
+import { svelteKitHandler } from "better-auth/svelte-kit";
+import { building } from '$app/environment'
+import { sequence } from '@sveltejs/kit/hooks';
 
 const handleParaglide: Handle = ({ event, resolve }) =>
 	paraglideMiddleware(event.request, ({ request, locale }) => {
@@ -14,4 +18,18 @@ const handleParaglide: Handle = ({ event, resolve }) =>
 		});
 	});
 
-export const handle: Handle = handleParaglide;
+const handleBetterAuth: Handle = async ({ event, resolve }) => {
+	const session = await auth.api.getSession({
+		headers: event.request.headers
+	});
+	if (session) {
+		event.locals.session = session.session;
+		event.locals.user = session.user;
+	} else {
+		event.locals.session = null;
+		event.locals.user = null;
+	}
+    return svelteKitHandler({ event, resolve, auth, building });
+};
+
+export const handle: Handle = sequence(handleParaglide, handleBetterAuth);
