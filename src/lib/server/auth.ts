1b5f1744050e1a02a5a688fa9f943d3863c663f1
jaubry-- <jaubry--@student.42lyon.fr>
2026-05-17 21:08:36 +0200

    fix(infra): fix auth missing secret and eslint auth.ts error

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -3,17 +3,15 @@ import { prismaAdapter } from 'better-auth/adapters/prisma';
 import db from '$lib/server/db';
 import { sveltekitCookies } from 'better-auth/svelte-kit';
 import { getRequestEvent } from '$app/server';
+import { env } from "$env/dynamic/private";
 import { building } from '$app/environment';
 
-let auth;
-if (!building) {
-	auth = betterAuth({
-		database: prismaAdapter(db, {
-			provider: 'sqlite'
-		}),
-		experimental: { joins: true },
-		emailAndPassword: { enabled: true },
-		plugins: [sveltekitCookies(getRequestEvent)]
-	});
-}
-export { auth };
+export const auth = betterAuth({
+	database: prismaAdapter(db, {
+		provider: 'sqlite'
+	}),
+	experimental: { joins: true },
+	emailAndPassword: { enabled: true },
+	plugins: [sveltekitCookies(getRequestEvent)],
+	secret: building ? "a" : env.BETTER_AUTH_SECRET,
+});
