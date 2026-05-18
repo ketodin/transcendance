640467bb83d6a3f496ab415465f851718bf42571
pabellis <mail@bellissantpablo.fr>
2026-05-18 20:46:44 +0200

    feat(front): create new layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,50 @@
+<!-- <script lang="ts"> -->
+<!-- 	import { ModeWatcher } from 'mode-watcher'; -->
+<!-- 	import Header from '$lib/components/Header.svelte'; -->
+<!-- 	import Footer from '$lib/components/Footer.svelte'; -->
+<!-- 	import '../app.css'; -->
+<!-- 	import * as Sidebar from '$lib/components/ui/sidebar/index.js'; -->
+<!-- 	import AppSidebar from '$lib/components/app-sidebar.svelte'; -->
+<!-- 	let { children } = $props(); -->
+<!-- </script> -->
+<!---->
+<!-- <ModeWatcher /> -->
+<!---->
+<!-- <div class="relative flex min-h-screen flex-col"> -->
+<!-- 	<Header /> -->
+<!---->
+<!-- 	<div class="flex-1"> -->
+<!-- 		<Sidebar.Provider> -->
+<!-- 			<div class="flex min-h-[calc(100vh-64px)] w-full"> -->
+<!-- 				<AppSidebar /> -->
+<!-- 				<main class="w-full flex-1 px-4 py-8"> -->
+<!-- 					<!-- eslint-disable-next-line @typescript-eslint/no-unsafe-call -->
+<!-- 					{@render children?.()} -->
+<!-- 				</main> -->
+<!-- 			</div> -->
+<!-- 		</Sidebar.Provider> -->
+<!-- 	</div> -->
+<!-- 	<Footer /> -->
+<!-- </div> -->
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+
+	import Friends from '$lib/components/Friends.svelte';
 	import '../app.css';
-	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import AppSidebar from '$lib/components/app-sidebar.svelte';
+
 	let { children } = $props();
 </script>
 
 <ModeWatcher />
 
-<div class="relative flex min-h-screen flex-col">
+<div class="app">
 	<Header />
 
-	<div class="flex-1">
-		<Sidebar.Provider>
-			<div class="flex min-h-[calc(100vh-64px)] w-full">
-				<AppSidebar />
-				<main class="w-full flex-1 px-4 py-8">
-					<!-- eslint-disable-next-line @typescript-eslint/no-unsafe-call -->
-					{@render children?.()}
-				</main>
-			</div>
-		</Sidebar.Provider>
-	</div>
+	<main class="game-container">
+		{@render children?.()}
+	</main>
 
-	<Footer />
+	<Friends />
 </div>
