54d2f6d3439b3adad6bb99f16d25f5062c961a32
Oliver Kientzler <okientzl@z4r10p7.42lyon.fr>
2026-05-19 16:29:11 +0200

    add constructor and destroy

--- src/lib/game/client/view/speechBubble.ts
+++ src/lib/game/client/view/speechBubble.ts
@@ -9,5 +9,23 @@ export class SpeechBubble {
 	private scene: Scene;
 	private timer?: Phaser.Time.TimerEvent;
 
-    
+	constructor(scene: Scene) {
+		this.scene = scene;
+		this.label = scene.add
+			.text(0, 0, '', {
+				fontSize: '13px',
+				color: COLOR_STRINGS.black,
+				wordWrap: { width: 160 },
+				align: 'center',
+			})
+			.setOrigin(0.5, 0.5)
+			.setDepth(51)
+			.setVisible(false);
+		this.bg = scene.add.graphics().setDepth(50);
+	}
+
+	destroy(): void {
+		this.bg.destroy();
+		this.label.destroy();
+	}
 }
