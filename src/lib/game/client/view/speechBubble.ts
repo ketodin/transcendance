f6fc90c791a7b0918cb7a7e6732787120da3da50
Oliver Kientzler <okientzl@z4r10p7.42lyon.fr>
2026-05-19 16:19:13 +0200

    create file and import

---
+++ src/lib/game/client/view/speechBubble.ts
@@ -0,0 +1,4 @@
+import { type TankState } from '$lib/game/shared/state/TankState';
+import { GameObjects, Scene } from 'phaser';
+import { COLORS, COLOR_STRINGS } from '$lib/game/phaser/colors';
+import { CHAT_BUBBLE_DURATION } from '$lib/game/shared/chatConfig';
