5fb2ed31b5bc353ccd947632504e7322000bfb97
Hikaru <98605870+Rev0li@users.noreply.github.com>
2026-05-28 19:07:27 +0200

    refactor(auth): move totp and start home page (#127)
    * refactor(auth): mouv totp and start home page
    
    * fix format
    
    * fix format
    
    * change message for close backup code
    
    ---------
    
    Co-authored-by: Oliver Kientzler <okientzl@z3r8p4.42lyon.fr>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,8 @@
 <script lang="ts">
-	import { useSession, signOut } from '$lib/auth-client';
 	import { Button } from '$lib/components/ui/button';
-
-	const session = useSession();
 </script>
 
-<div class="flex flex-col gap-16">
-	<div>
-		{#if $session.data}
-			{#each Object.entries($session.data.user) as [key, value] (key)}
-				<p><strong>{key}:</strong> {value}</p>
-			{/each}
-			<Button
-				class="glass"
-				onclick={async () => {
-					await signOut();
-				}}>Logout</Button
-			>
-		{:else}
-			<p>Not logged in</p>
-		{/if}
-	</div>
+<div class="flex flex-col items-center justify-center gap-8">
+	<h1 class="text-4xl font-bold">Bienvenue sur tank game</h1>
+	<Button href="/game" class="h-auto px-12 py-6 text-2xl font-bold">PLAY</Button>
 </div>
