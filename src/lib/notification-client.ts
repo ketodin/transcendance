d14ce158c5d6511ccf49d0f7d37bc4c3ac1a89f9
Timeo Daclin <tdaclin@student.42lyon.fr>
2026-06-01 16:44:16 +0200

    feat(front): reload friend sidebar when receiving friend request

--- src/lib/notification-client.ts
+++ src/lib/notification-client.ts
@@ -1,12 +1,22 @@
 import { toast } from '$lib/components/toast';
 import type { Room } from '@colyseus/sdk';
 import { m } from '$lib/paraglide/messages';
+import { list as getFriendList } from '$lib/friends.remote';
 
 export function attachNotificationListeners(room: Room) {
 	const NOTIF_FRIEND: string = 'notification_friend_request';
-	room.onMessage(`${NOTIF_FRIEND}_received`, () => toast.info(m.friend_request_received()));
-	room.onMessage(`${NOTIF_FRIEND}_accepted`, () => toast.success(m.friend_request_accepted()));
-	room.onMessage(`${NOTIF_FRIEND}_denied`, () => toast.warning(m.friend_request_denied()));
+	room.onMessage(`${NOTIF_FRIEND}_received`, () => {
+		void getFriendList().refresh();
+		toast.info(m.friend_request_received());
+	});
+	room.onMessage(`${NOTIF_FRIEND}_accepted`, () => {
+		void getFriendList().refresh();
+		toast.success(m.friend_request_accepted());
+	});
+	room.onMessage(`${NOTIF_FRIEND}_denied`, () => {
+		void getFriendList().refresh();
+		toast.warning(m.friend_request_denied());
+	});
 
 	const NOTIF_INVITE: string = 'notification_invite_request';
 	room.onMessage(`${NOTIF_INVITE}_received`, () => toast.info(m.game_invite_received()));
