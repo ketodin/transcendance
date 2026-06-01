1481940e4795a1fb4ffec1f640976145dc3ce382
Timeo Daclin <tdaclin@student.42lyon.fr>
2026-06-01 19:41:57 +0200

    fix(docker): make file upload work with docker volumes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,8 @@ set -eu
 # initialize the database
 pnpm run db:push --url "$DATABASE_URL"
 
+# store avatar in $AVATAR_DIR
+rm -rf ./static/avatar && mkdir -p "$AVATAR_DIR"
+ln -sn "$AVATAR_DIR" ./static/avatar
+
 exec "$@"
