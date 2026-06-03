cdcdd6b9ea6daedd97ab1689b48f1a6552ab1c33
pabellis <mail@bellissantpablo.fr>
2026-06-03 17:22:29 +0200

    feat(i18n): add messages

--- src/routes/settings/schema.ts
+++ src/routes/settings/schema.ts
@@ -7,16 +7,16 @@ const ACCEPTED_TYPES = ['image/png', 'image/jpeg'];
 export const avatarSchema = z.strictObject({
 	file: z
 		.instanceof(File, {
-			message: 'Please upload a file' // TODO: i18n
+			message: m.please_upload_file()
 		})
 		.refine((f) => f.size > 0, 'File is required')
 		.refine(
 			(f) => f.size <= MAX_FILE_SIZE,
-			'Max 5MB upload size' // TODO: i18n
+			m.max_upload_size()
 		)
 		.refine(
 			(f) => ACCEPTED_TYPES.includes(f.type),
-			'Only PNG and JPEG images are allowed' // TODO: i18n
+			m.unknow_upload_file()
 		)
 });
 
