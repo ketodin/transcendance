44bb90ac61a22bc56503aa9dfbd5cd0d49052648
Timeo Daclin <tdaclin@student.42lyon.fr>
2026-06-07 15:52:09 +0200

    feat(front): add invite system with notifications

--- src/routes/profile/[id]/+page.server.ts
+++ src/routes/profile/[id]/+page.server.ts
@@ -1,7 +1,11 @@
-import type { PageServerLoad } from './$types';
-import { error } from '@sveltejs/kit';
+import type { PageServerLoad, Actions } from './$types';
+import { error, fail } from '@sveltejs/kit';
 import db from '$lib/server/db';
 import { m } from '$lib/paraglide/messages';
+import { matchMaker } from 'colyseus';
+import { redirect } from '@sveltejs/kit';
+import { resolve } from '$app/paths';
+import { statusHub } from '$lib/game/colyseus/statusHub';
 
 export const load: PageServerLoad = async ({ params }) => {
 	const user = await db.user.findUnique({
@@ -9,4 +13,18 @@ export const load: PageServerLoad = async ({ params }) => {
 	});
 	if (!user) return error(404, m.no_user());
 	return { userProfile: user };
+};
+
+export const actions: Actions = {
+	invitePrivateGame: async ({ request, locals }) => {
+		const formData = await request.formData()
+		const userId = formData.get('userId');
+		if (!userId || typeof userId !== 'string')
+			return fail(400, "userId must be a string");
+
+		const room = await matchMaker.createRoom('tank_room', { private: true });
+		statusHub.notify(userId, 'invite_request_received', { fromUserId: locals.user!.id, fromUserName: locals.user!.name, roomId: room.roomId });
+
+		return redirect(303, resolve('/game/[[id]]', { id: room.roomId }));
+	},
 };
