d30c75b96223886ab383582e4845d798ae589739
ketodin <69580109+ketodin@users.noreply.github.com>
2026-06-09 17:23:35 +0200

    feat(shared): implement game invite notifications (#188)
    * style(i18n): remove useless hello_world translation
    
    * feat(i18n): remove useless translation and udate ttranslation to use `{name}`
    
    * feat(backend): add name specification in toast
    add difference between deny and remove friend notification
    
    * style(i18n): format translations
    
    * feat(frontend): add error code 410 (room no longer exist) to use error theme
    
    * feat(frontend): add redirection with ecode 410 when room doesnt exist
    
    * feat(shared): implement notifications for game invites properly
    
    * feat(shared): remove server action and replace form with button for game invite send
    
    * style(global): fix format and lint errors
    
    * refactor(front): switch to boolean instead of 0 or 1
    
    * feat(shared): handle correctly colyseus errors on the client side
    
    ---------
    
    Co-authored-by: Timeo Daclin <tdaclin@student.42lyon.fr>

--- src/lib/notification-client.ts
+++ src/lib/notification-client.ts
@@ -4,6 +4,9 @@ import { m } from '$lib/paraglide/messages';
 import { list as getFriendList } from '$lib/friends.remote';
 import { goto } from '$app/navigation';
 import { resolve } from '$app/paths';
+import * as invite from '$lib/invite.remote';
+import { applyAction } from '$app/forms';
+import type { HttpError } from '@sveltejs/kit';
 
 export function attachNotificationListeners(room: Room) {
 	const NOTIF_FRIEND: string = 'notification_friend_request';
@@ -11,33 +14,60 @@ export function attachNotificationListeners(room: Room) {
 		void getFriendList().refresh();
 		toast.info(m.friend_request_received());
 	});
-	room.onMessage(`${NOTIF_FRIEND}_accepted`, () => {
+	room.onMessage(`${NOTIF_FRIEND}_accepted`, ({ fromUserName }: { fromUserName: string }) => {
 		void getFriendList().refresh();
-		toast.success(m.friend_request_accepted());
+		toast.success(m.friend_request_accepted({ name: fromUserName }));
 	});
-	room.onMessage(`${NOTIF_FRIEND}_denied`, () => {
+	room.onMessage(`${NOTIF_FRIEND}_denied`, ({ fromUserName }: { fromUserName: string }) => {
 		void getFriendList().refresh();
-		toast.warning(m.friend_request_denied());
+		toast.warning(m.friend_request_denied({ name: fromUserName }));
 	});
+	room.onMessage(`${NOTIF_FRIEND}_removed`, () => void getFriendList().refresh());
 
 	const NOTIF_INVITE: string = 'notification_invite_request';
 	room.onMessage(
 		`${NOTIF_INVITE}_received`,
-		({ fromUserName, roomId }: { fromUserId: string; fromUserName: string; roomId: string }) => {
-			toast.info(m.game_invite_received() + fromUserName, {
+		({
+			fromUserId,
+			fromUserName,
+			roomId
+		}: {
+			fromUserId: string;
+			fromUserName: string;
+			roomId: string;
+		}) => {
+			toast.info(m.game_invite_received({ name: fromUserName }), {
 				action: {
 					label: ' ✔ ',
 					onClick: async () => {
+						try {
+							await invite.accept({ roomId, toUserId: fromUserId });
+						} catch (err) {
+							const error = err as HttpError;
+							return await applyAction({
+								type: 'error',
+								status: error.status,
+								error: { message: error.body.message }
+							});
+						}
 						await goto(resolve('/game/[[id]]', { id: roomId }), { invalidateAll: true });
 					}
 				},
 				cancel: {
 					label: ' ✖ ',
-					onClick: () => {}
-				}
+					onClick: async () => {
+						await invite.deny({ toUserId: fromUserId });
+					}
+				},
+				duration: Infinity
 			});
 		}
 	);
-	room.onMessage(`${NOTIF_INVITE}_accepted`, () => toast.success(m.game_invite_accepted()));
-	room.onMessage(`${NOTIF_INVITE}_denied`, () => toast.warning(m.game_invite_denied()));
+	room.onMessage(`${NOTIF_INVITE}_accepted`, ({ fromUserName }: { fromUserName: string }) =>
+		toast.success(m.game_invite_accepted({ name: fromUserName }))
+	);
+	room.onMessage(`${NOTIF_INVITE}_denied`, ({ fromUserName }: { fromUserName: string }) => {
+		toast.warning(m.game_invite_denied({ name: fromUserName }));
+		void goto(resolve('/'));
+	});
 }
