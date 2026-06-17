beab9feeaf1bbf86c0ce2f3a8a54f23b0adb1411
Oliver Kientzler <okientzl@z3r5p6.42lyon.fr>
2026-06-17 17:56:54 +0200

    security(auth): add option ratelimit

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -22,11 +22,11 @@ export const auth = betterAuth({
 	database: prismaAdapter(db, {
 		provider: 'sqlite'
 	}),
-	trustedOrigins: (request) => {
-		if (!request) return [];
-		const origin = request.headers.get('origin');
-		return origin ? [origin] : [];
-	},
+	rateLimit: {
+		enabled: true,
+        window: 60, // time window in seconds
+        max: 100, // max requests in the window
+    },
 	experimental: { joins: true },
 	emailAndPassword: { enabled: true },
 	hooks: {
