2e71f1c3881b1c2754f56f32dc2c2d8430e184e4
Timeo Daclin <tdaclin@student.42lyon.fr>
2026-06-18 17:02:34 +0200

    refactor(front): better error handling

---
+++ src/hooks.client.ts
@@ -0,0 +1,5 @@
+import type { HandleClientError } from '@sveltejs/kit';
+
+export const handleError: HandleClientError = ({ error }) => {
+	return { message: error instanceof Error ? error.message : String(error) };
+};
--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -89,7 +89,7 @@
 	async function handleDelete() {
 		await friends.notifyRemoval();
 		await authClient.deleteUser();
-		window.location.pathname = "/register"
+		window.location.pathname = '/register';
 	}
 </script>
 
