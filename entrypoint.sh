0187c78f513417f0d39b5563b64e152b63c0c42f
42Timeo <tdaclin@student.42lyon.fr>
2026-06-19 16:37:41 +0200

    fix(infra): fix the entrypoint.sh script (#297)

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -eu
 pnpm run db:push --url "$DATABASE_URL"
 
 # store avatar in $AVATAR_DIR
-rm -rf ./static/avatar && mkdir -p "$AVATAR_DIR"
+rm -rf ./static/uploads && mkdir -p "$AVATAR_DIR"
 ln -sn "$AVATAR_DIR" ./static/uploads
 
 exec "$@"
